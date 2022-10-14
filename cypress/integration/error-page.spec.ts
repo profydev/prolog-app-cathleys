715650db8a3e8f4f80f73ef2bc806cd63477817a
cath'scoder <108257596+cathleys@users.noreply.github.com>
2022-10-14 16:38:52 +0800

    Fixes error page test

--- cypress/integration/error-page.spec.ts
+++ cypress/integration/error-page.spec.ts
@@ -1,5 +1,7 @@
 describe("Error Page", () => {
   context("Error", () => {
+    const ErrorMsg = "There was a problem while loading the project data";
+
     it("simulates an error when dashboard cannot be displayed", () => {
       cy.intercept(
         {
@@ -14,7 +16,7 @@ describe("Error Page", () => {
       cy.visit("http://localhost:3000/dashboard");
       cy.wait("@getNetworkFailure");
       cy.wait(10000);
-      cy.get(".error-style__Container-sc-87a2b9f7-0").should("be.visible");
+      cy.contains(ErrorMsg).should("be.visible");
     });
   });
 });
