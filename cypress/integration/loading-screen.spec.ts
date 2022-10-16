4125063faf38de8c6d010d93553648e5b52f6000
cath'scoder <108257596+cathleys@users.noreply.github.com>
2022-10-16 16:30:15 +0800

    Fixes the test for the 3rd time.

--- cypress/integration/loading-screen.spec.ts
+++ cypress/integration/loading-screen.spec.ts
@@ -6,12 +6,9 @@ describe("loads the page", () => {
     });
 
     cy.visit("http://localhost:3000/dashboard");
-    cy.get(".loading-screen-style__Container-sc-82d1c511-0").should(
-      "be.visible"
-    );
-    cy.get(".loading-screen-style__Container-sc-82d1c511-0").should(
-      "not.exist"
-    );
+
+    cy.get(".loading-screen-style__Loader-sc-82d1c511-1").should("be.visible");
+    cy.get(".loading-screen-style__Loader-sc-82d1c511-1").should("not.exist");
     cy.get("main").find("li").should("have.length", 3);
   });
 });
