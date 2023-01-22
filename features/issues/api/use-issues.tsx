04f85b3177b622f7d2f4a6c103caa0a7b342b334
cath'scoder <108257596+cathleys@users.noreply.github.com>
2023-01-22 17:34:53 +0800

    Adds status param and filter data with react-query

--- features/issues/api/use-issues.tsx
+++ features/issues/api/use-issues.tsx
@@ -2,30 +2,33 @@ import { useEffect } from "react";
 import { useQuery, useQueryClient } from "react-query";
 import axios from "axios";
 import type { Page } from "@typings/page.types";
-import type { Issue } from "../types/issue.types";
+import { Issue } from "../types/issue.types";
 
-async function getIssues(page: number) {
+async function getIssues(page: number, status: string) {
   const { data } = await axios.get(
-    `https://prolog-api.profy.dev/issue?page=${page}`
+    `https://prolog-api.profy.dev/issue?page=${page}&status=${status}`
   );
+
   return data;
 }
 
-export function useIssues(page: number) {
+export function useIssues(page: number, optionStatus: string) {
   const query = useQuery<Page<Issue>, Error>(
-    ["issues", page],
-    () => getIssues(page),
+    ["issues", page, optionStatus],
+    () => getIssues(page, optionStatus),
     { keepPreviousData: true, staleTime: 60000 }
   );
 
   // Prefetch the next page!
   const queryClient = useQueryClient();
+
   useEffect(() => {
     if (query.data?.meta.hasNextPage) {
       queryClient.prefetchQuery(["projects", page + 1], () =>
-        getIssues(page + 1)
+        getIssues(page + 1, optionStatus)
       );
     }
   }, [query.data, page, queryClient]);
+
   return query;
 }
