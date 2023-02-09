909e1744f2e23e644e031eef0aeb3b98f9032580
cath'scoder <108257596+cathleys@users.noreply.github.com>
2023-02-09 12:51:17 +0800

    Adds testing

--- cypress/integration/dropdown.spec.ts
+++ cypress/integration/dropdown.spec.ts
@@ -1,49 +1,57 @@
-describe("React-select dropdowns", () => {
+describe("React-select and search bar", () => {
   beforeEach(() => {
     cy.visit(`http://localhost:3000/dashboard/issues`);
   });
 
   it("selects the status dropdown", () => {
     //add input for the react-select to work
-    cy.get(":nth-child(1) > .css-wd1q99-control input")
-      .first()
-      .type("U")
+    cy.get(".issue-list__WrapperStyle-sc-6917d16d-0")
+      .get(":nth-child(1) > .css-wd1q99-control input")
+      .click({ force: true })
       .get(".css-1n6sfyn-MenuList")
+      .find("#react-select-status-dropdown-value-option-1")
       .contains("Unresolved")
       .click();
 
-    cy.get(":nth-child(1) > .css-wd1q99-control input")
-      .first()
-      .type("r")
+    cy.get(".issue-list__WrapperStyle-sc-6917d16d-0")
+      .get(":nth-child(1) > .css-wd1q99-control input")
+      .click({ force: true })
       .get(".css-1n6sfyn-MenuList")
+      .find("#react-select-status-dropdown-value-option-2")
       .contains("Resolved")
       .click();
 
-    cy.get(":nth-child(1) > .css-wd1q99-control input")
-      .click()
+    cy.get(".issue-list__WrapperStyle-sc-6917d16d-0")
+      .get(":nth-child(1) > .css-wd1q99-control input")
+      .click({ force: true })
       .get(".css-1n6sfyn-MenuList")
+      .get("#react-select-status-dropdown-value-option-0")
       .contains("--")
       .click();
   });
 
   it("selects the level dropdown", () => {
-    cy.get(":nth-child(2) > .css-wd1q99-control input")
-      .first()
-      .type("e")
+    cy.get(".issue-list__WrapperStyle-sc-6917d16d-0")
+      .get(":nth-child(2) > .css-wd1q99-control input")
+      .click({ force: true })
       .get(".css-1n6sfyn-MenuList")
+      .find("#react-select-level-dropdown-value-option-1")
       .contains("Error")
       .click();
 
-    cy.get(":nth-child(2) > .css-wd1q99-control input")
-      .first()
-      .type("w")
+    cy.get(".issue-list__WrapperStyle-sc-6917d16d-0")
+      .get(":nth-child(2) > .css-wd1q99-control input")
+      .click({ force: true })
       .get(".css-1n6sfyn-MenuList")
+      .find("#react-select-level-dropdown-value-option-2")
       .contains("Warning")
       .click();
 
-    cy.get(":nth-child(2) > .css-wd1q99-control input")
-      .click()
+    cy.get(".issue-list__WrapperStyle-sc-6917d16d-0")
+      .get(":nth-child(2) > .css-wd1q99-control input")
+      .click({ force: true })
       .get(".css-1n6sfyn-MenuList")
+      .find("#react-select-level-dropdown-value-option-0")
       .contains("--")
       .click();
   });
@@ -66,6 +74,7 @@ describe("React-select dropdowns", () => {
       .clear()
       .type("ml service")
       .should("have.value", "ml service")
+      .clear()
       .blur();
   });
 });
