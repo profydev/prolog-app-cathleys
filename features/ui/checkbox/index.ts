2a6b6f565fff0389605191f36168bac18ca4d9f3
cath'scoder <108257596+cathleys@users.noreply.github.com>
2023-02-22 17:50:09 +0800

    Tests the indeterminate checkbox on storybook

--- features/ui/checkbox/index.ts
+++ features/ui/checkbox/index.ts
@@ -1,1 +1,4 @@
 export { Checkbox, CheckboxSize, CheckboxState } from "./checkbox";
+export * from "./checkbox.style";
+export { NewCheckbox } from "./new-checkbox";
+export * from "./new-checkbox.style";
